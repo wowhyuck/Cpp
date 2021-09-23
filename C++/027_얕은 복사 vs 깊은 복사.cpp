5ec6d554e05160bea115ac72f4f411824619dfca
Joohyuck <joohyuck.seo@gmail.com>
2021-09-24 04:25:13 +0900

    얕은 복사 vs 깊은 복사 #2

--- C++/027_얕은 복사 vs 깊은 복사.cpp
+++ C++/027_얕은 복사 vs 깊은 복사.cpp
@@ -21,37 +21,70 @@ public:
 	{
 		cout << "~Pet()" << endl;
 	}
+
+	Pet& operator=(const Pet& pet)
+	{
+		cout << "operator(const Pet&)" << endl;
+		return *this;
+	}
 };
 
-class Knight
+class Player
+{
+public:
+	Player()
+	{
+		cout << "Player()" << endl;
+	}
+
+	Player(const Player& player)
+	{
+		cout << "Player(const Player&)" << endl;
+		_level = player._level;
+	}
+
+	Player& operator=(const Player& player)
+	{
+		cout << "operator=(const Player&)" << endl;
+		_level = player._level;
+		return *this;
+	}
+
+public:
+	int _level = 99;
+};
+
+class Knight : public Player
 {
 public:
 	Knight()
 	{
-		_pet = new Pet();
+
 	}
 
-	Knight(const Knight& knight)
+	Knight(const Knight& knight) : Player(knight), _pet(knight._pet)
 	{
+		cout << "Knight(const Knight&)" << endl;
 		_hp = knight._hp;
-		_pet = new Pet(*knight._pet);		// 깊은 복사
 	}
 
 	Knight& operator=(const Knight& knight)
 	{
+		cout << "operator=(const Knight&)" << endl;
+
+		Player::operator=(knight);
+		_pet = knight._pet;
 		_hp = knight._hp;
-		_pet = new Pet(*knight._pet);		// 깊은 복사
 		return *this;
 	}
 
 	~Knight()
 	{
-		delete _pet;
 	}
 
 public:
 	int _hp = 100;
-	Pet* _pet;
+	Pet _pet;
 };
 
 int main()
@@ -59,10 +92,12 @@ int main()
 	Knight knight;		// 기본 생성자
 	knight._hp = 200;
 
+	//cout << "-------------- 복사 생성자 --------------" << endl;
 	Knight knight2 = knight;	// 복사 생성자
 	//Knight knight3(knight);
 
 	Knight knight3;		// 기본 생성자
+	cout << "-------------- 복사 대입 연산자 --------------" << endl;
 	knight3 = knight;	// 복사 대입 연산자
 
 	// [복사 생성자] + [복사 대입 연산자]
@@ -81,6 +116,28 @@ int main()
 	// Stack : Knight1[ hp 0x1000 ] -> Heap 0x1000 Pet[ ]
 	// Stack : Knight2[ hp 0x2000 ] -> Heap 0x2000 Pet[ ]
 	// Stack : Knight3[ hp 0x3000 ] -> Heap 0x3000 Pet[ ]
+
+	// 실험)
+	// - 암시적 복사 생성자 Steps
+	// 1) 부모 클래스의 복사 생성자 호출
+	// 2) 멤버 클래스의 복사 생성자 호출
+	// 3) 멤버가 기본 타입일 경우 메모리 복사 (얕은 복사 Shallow Copy)
+	// - 명시적 복사 생성자 Steps
+	// 1) 부모 클래스의 기본 생성자 호출
+	// 2) 멤버 클래스의 기본 생성자 호출
+
+	// - 암시적 복사 대입 연산자 Steps
+	// 1) 부모 클래스의 복사 대입 연산자 호출
+	// 2) 멤버 클래스의 복사 대입 연산자 호출
+	// 3) 멤버가 기본 타입일 경우 메모리 복사 (얕은 복사 Shallow Copy)
+	// - 명시적 복사 대입 연산자 Steps
+	// 1) 알아서 해주는거 없음
+
+	// 왜 이렇게 혼란스러울까?
+	// 객체를 '복사' 한다는 것은 두 객체의 값들을 일치시키려는 것
+	// 따라서 기본적으로 얕은 복사 (Shallow Copy) 방식으로 동작
+
+	// 명시적 복사 -> [모든 책임]을 프로그래머한테 위임하겠다는 의미
 
 	return 0;
 }
