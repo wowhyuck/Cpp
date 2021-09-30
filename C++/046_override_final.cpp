263458872fe4c6d747055ac2f23ea3444eafee66
Joohyuck <joohyuck.seo@gmail.com>
2021-09-30 18:26:16 +0900

    override, final

---
+++ C++/046_override_final.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// 오늘의 주제 : override, final
+
+class Creature
+{
+public:
+	virtual void Attack()
+	{
+		cout << "Creature" << endl;
+	}
+};
+
+class Player : public Creature
+{
+public:
+	virtual void Attack() override
+	{
+		cout << "Player" << endl;
+	}
+};
+
+
+class Knight : public Player
+{
+public:
+	// 재정의 (override)
+	virtual void Attack() final
+	{
+		cout << "Knight" << endl;
+	}
+
+private:
+	int _stamina = 100;
+};
+
+
+int main()
+{
+	Player* p = new Knight();
+	p->Attack();
+
+	return 0;
+}
