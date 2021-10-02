2cd604e3a62cbde9c812de46385de443c8ee1ca1
Joohyuck <joohyuck.seo@gmail.com>
2021-10-03 02:48:30 +0900

    스마트 포인터 (smart pointer)

---
+++ C++/050_스마트 포인터.cpp
@@ -0,0 +1,125 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// 오늘의 주제 : 스마트 포인터 (smart pointer)
+
+class Knight
+{
+public:
+	Knight() { cout << "Knight 생성" << endl; }
+	~Knight() { cout << "Knight 소멸" << endl; }
+
+	void Attack()
+	{
+		if (_target)
+		{
+			_target->_hp -= _damage;
+			cout << "HP: " << _target->_hp << endl;
+		}
+	}
+
+public:
+	int _hp = 100;
+	int _damage = 10;
+	shared_ptr<Knight> _target = nullptr;
+};
+
+class RefCountBlock
+{
+public:
+	int _refCount = 1;
+};
+
+template<typename T>
+class SharedPtr
+{
+public:
+	SharedPtr() {}
+	SharedPtr(T* ptr) : _ptr(ptr)
+	{
+		if (_ptr != nullptr)
+		{
+			_block = new RefCountBlock();
+			cout << "RefCount : " << _block->_refCount << endl;
+		}
+	}
+
+	SharedPtr(const SharedPtr& sptr) : _ptr(sptr._ptr), _block(sptr._block)
+	{
+		if (_ptr != nullptr)
+		{
+			_block->_refCount++;
+			cout << "RefCount : " << _block->_refCount << endl;
+		}
+	}
+
+	void operator=(const SharedPtr& sptr)
+	{
+		_ptr = sptr._ptr;
+		_block = sptr._block;
+
+		if (_ptr != nullptr)
+		{
+			_block->_refCount++;
+			cout << "RefCount : " << _block->_refCount << endl;
+		}
+	}
+
+	~SharedPtr()
+	{
+		if (_ptr != nullptr)
+		{
+			_block->_refCount--;
+			cout << "RefCount : " << _block->_refCount << endl;
+
+			if (_block->_refCount == 0)
+			{
+				delete _ptr;
+				delete _block;
+				cout << "Delete Data" << endl;
+			}
+		}
+	}
+
+public:
+	T* _ptr = nullptr;
+	RefCountBlock* _block = nullptr;
+};
+
+
+int main()
+{
+	//Knight* k1 = new Knight();
+	//Knight* k2 = new Knight();
+
+	//k1->_target = k2;
+	//k1->Attack();
+
+	// 스마트 포인터 : 포인터를 알맞는 정책에 따라 관리하는 객체 (포인터를 래핑해서 사용)
+	// share_ptr, weak_ptr, unique_ptr
+
+	//SharedPtr<Knight> k2;
+
+	//{
+	//	SharedPtr<Knight> k1(new Knight());
+	//	k2 = k1;
+	//}
+
+	shared_ptr<Knight> k1 = make_shared<Knight>();
+	shared_ptr<Knight> k2 = make_shared<Knight>();
+
+	k1->_target = k2;
+	k2->_target = k1;
+
+	k1->Attack();
+
+	k1->_target = nullptr;
+	k2->_target = nullptr;
+
+
+	unique_ptr<Knight> uptr = make_unique<Knight>();
+	unique_ptr<Knight> uptr2 = std::move(uptr);
+
+	return 0;
+}
